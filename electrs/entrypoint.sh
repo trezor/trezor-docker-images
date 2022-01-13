62f48e855fbcb4fb81c00ac6682ff0a3276a1d55
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-01-13 16:50:50 +0100

    feat: connect to bitcoind with cookie file

--- electrs/entrypoint.sh
+++ electrs/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo -e "\nStarting bitcoin node.\n"
-/root/bitcoind -regtest -server -fallbackfee=0.0002 -rpcallowip=0.0.0.0/0 -rpcbind=0.0.0.0 -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc -blockfilterindex=1 -peerblockfilters=1 
+/root/bitcoind -regtest -server -daemon --rpccookiefile=/root/.cookie -fallbackfee=0.0002 -rpcallowip=0.0.0.0/0 -rpcbind=0.0.0.0 -rpcport=18021 -blockfilterindex=1 -peerblockfilters=1
 
 echo -e "\nWaiting for bitcoin node.\n"
-until /root/bitcoin-cli -regtest -datadir=/root/.bitcoin -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc getblockchaininfo; do
+until /root/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 getblockchaininfo; do
     sleep 1
 done
 echo -e "\nCreate tenv-test wallet.\n"
-/root/bitcoin-cli -regtest -datadir=/root/.bitcoin -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc createwallet tenv-test
+/root/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 createwallet tenv-test
 
 echo -e "\nGenerating 150 bitcoin blocks.\n"
-ADDR=$(/root/bitcoin-cli -regtest -datadir=/root/.bitcoin -rpcwallet=tenv-test getnewaddress)
-/root/bitcoin-cli -regtest -datadir=/root/.bitcoin -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc generatetoaddress 150 $ADDR
+ADDR=$(/root/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 -rpcwallet=tenv-test getnewaddress)
+/root/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 generatetoaddress 150 $ADDR
 
 echo -e "\nStarting electrs node.\n"
-/root/electrs --network regtest --txid-limit 0 -vvv --jsonrpc-import
+/root/electrs --log-filters INFO --db-dir /tmp/electrs-db --electrum-rpc-addr="0.0.0.0:50001" --network=regtest --cookie-file=/root/.cookie
