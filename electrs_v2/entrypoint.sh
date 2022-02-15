c80d53da554e6725e8ad0389164de2d3a14d652d
vdovhanych <dovhanych@me.com>
2022-02-15 11:38:14 +0100

    fix adding the addres to elecrts

--- electrs_v2/entrypoint.sh
+++ electrs_v2/entrypoint.sh
@@ -11,7 +11,7 @@ echo -e "\nCreate tenv-test wallet.\n"
 /usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 createwallet tenv-test
 
 echo -e "\nGenerating 150 bitcoin blocks.\n"
-ADDR=$(/root/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 -rpcwallet=tenv-test getnewaddress)
+ADDR=$(/usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 -rpcwallet=tenv-test getnewaddress)
 /usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 generatetoaddress 150 $ADDR
 
 # echo -e "\nStarting electrs node.\n"
