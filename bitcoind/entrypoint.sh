b211fcbb78fbd3b76bcec10a75d0c61f20cf49d2
vdovhanych <dovhanych@me.com>
2022-02-15 22:57:59 +0100

    editorconfig styling fix

--- bitcoind/entrypoint.sh
+++ bitcoind/entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo -e "Starting bitcoin node."
-/root/bitcoind -regtest -server -fallbackfee=0.0002 -rpcallowip=0.0.0.0/0 -rpcbind=0.0.0.0 -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc -blockfilterindex=1 -peerblockfilters=1 
+/root/bitcoind -regtest -server -fallbackfee=0.0002 -rpcallowip=0.0.0.0/0 -rpcbind=0.0.0.0 -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc -blockfilterindex=1 -peerblockfilters=1
