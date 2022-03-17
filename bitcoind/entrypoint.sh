01e7742d091a874c6909cf5a4de5450b10c7f09f
vdovhanych <dovhanych@me.com>
2022-03-17 12:23:32 +0100

    feat: migrate from personal repo and delete unused bitcoind

--- bitcoind/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo -e "Starting bitcoin node."
-/root/bitcoind -regtest -server -fallbackfee=0.0002 -rpcallowip=0.0.0.0/0 -rpcbind=0.0.0.0 -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc -blockfilterindex=1 -peerblockfilters=1
