bf84188a7cd52ab030807d493f3da64f953bbc26
vdovhanych <dovhanych@me.com>
2022-12-09 16:10:38 +0100

    feat: add blockbook-regtest iamge

---
+++ blockbook-regtest/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# start bitcoin backend
+cd /opt/coins/nodes/bitcoin_regtest
+
+echo "Starting bitcoin regtest backend service"
+bin/bitcoind -blockfilterindex -datadir=/opt/coins/data/bitcoin_regtest/backend -conf=/opt/coins/nodes/bitcoin_regtest/bitcoin_regtest.conf &
+
+sleep 5
+
+# generate test wallet and blocks
+bin/bitcoin-cli -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc createwallet "tenv-wallet"
+bin/bitcoin-cli -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc -generate 150
+bin/bitcoin-cli -rpcport=18021 -rpcuser=rpc -rpcpassword=rpc settxfee 0.00001
+
+# start blockbook
+cd /opt/coins/blockbook/bitcoin_regtest
+
+echo "Starting blockbook service"
+bin/blockbook \
+    -blockchaincfg=/opt/coins/blockbook/bitcoin_regtest/config/blockchaincfg.json \
+    -datadir=/opt/coins/data/bitcoin_regtest/blockbook/db \
+    -sync \
+    -internal=:19021 \
+    -public=:19121 \
+    -explorer= \
+    -log_dir=/opt/coins/blockbook/bitcoin_regtest/logs \
+    -dbcache=0
